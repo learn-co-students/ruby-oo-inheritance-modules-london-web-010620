c8d23891230002c59aeafa51569ab8744786006b
Wojciech Bojarski <bojawojt@gmail.com>
2020-01-15 13:59:26 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,15 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require './fancy_dance.rb'
+
+class Dancer
+    #include enables us to access all the methods contained within the module specified
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
